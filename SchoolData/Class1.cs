54b0a61e8db92fdf74de28fad7f82cc38005dc61
ammwangi <elijah.mwangi@kamsware.com>
2022-06-30 14:55:50 +0300

    Add project files.

---
+++ SchoolData/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SchoolData
+{
+    public class Class1
+    {
+    }
+}
---
+++ SchoolDomain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SchoolDomain
+{
+    public class Class1
+    {
+    }
+}
